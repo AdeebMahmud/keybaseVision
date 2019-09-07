cb7f064d98556c1192216189b21fee0ed39bc21a
Adeeb Mahmud <mahmudadeeb@gmail.com>
2019-09-07 18:33:41 -0400

    keybaseVision ver 1.

--- bot.js
+++ bot.js
@@ -1,12 +1,15 @@
-const Bot = require('keybase-bot')
-const wiki = require("wikiquote");
-const username = 'keybasevision'
-const logger = require()
+const Bot = require('keybase-bot');
+const wiki = require("wikijs").default;
+const logger = require("winston");
 const paperkey = require('./creds.json');
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
+const username = 'keybasevision'
+// Imports the Google Cloud client library
+const vision = require('@google-cloud/vision');
+
+// Configure logger settings                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+logger.remove(logger.transports.Console);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+logger.add(new logger.transports.Console, {                                                                     
     colorize: true
 });
 logger.level = 'debug';
@@ -14,21 +17,56 @@ logger.level = 'debug';
 // Initialize Bot
 const keybaseVision = new Bot();
 
-bot
-  .init(username, paperkey, {verbose: false})
-  .then(() => {
-    logger.info(`Your bot is initialized. It is logged in as ${bot.myInfo().username}`)
+async function getEmbed(image) {
 
-    bot.chat.watchAllChannelsForNewMessages(message => {
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+  // Performs label detection on the image file
+  logger.info("getting labels");
+  const [result] = await client.labelDetection(image);
+  const labels = result.labelAnnotations;
+  logger.info('Labels:');
+  return labels;
+}
+
+async function getDescription(labels) {
+    outputString = "We think it could be: " + labels.map(label => label.description).join(', ');
+
+    const page = await wiki().find(labels[0].description);
+
+    const summary = await page.summary()
+    
+    outputString += summary;
+
+    return outputString
+}
+
+keybaseVision                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+  .init(username, paperkey.paperkey, {verbose: false})
+  .then(() => {
+    logger.info(`Your bot is initialized. It is logged in as ${keybaseVision.myInfo().username}`)
+
+    keybaseVision.chat.watchAllChannelsForNewMessages(message => {
         const channel = message.channel;
-        if(message.content.type === "attatchment") {
-            bot.chat.download(channel, message.id, "./current.png");
+        if(message.content.type === "attachment") {
+            keybaseVision.chat.download(channel, message.id, "./current.jpg")
+                .catch(error => {
+                    console.error(error)
+                })
         }
     })
 
-    }
+    keybaseVision.chat.watchAllChannelsForNewMessages(message => {
+        const channel = message.channel;
+        if (message.content.text.body === "!classify") {
+            getEmbed("./current.jpg")
+                .then(labels => getDescription(labels)).then(summary => 
+                    keybaseVision.chat.send(channel, {body: summary}))
+        }
+    })
   })
   .catch(error => {
     console.error(error)
-    bot.deinit()
+    keybaseVision.deinit()
   })
+  
